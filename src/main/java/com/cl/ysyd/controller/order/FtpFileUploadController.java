efdce53e3691259532087d53fbf710f0eca25305
cl1203 <550168205@qq.com>
2020-12-27 23:08:12 +0800

    上传优化

--- src/main/java/com/cl/ysyd/controller/order/FtpFileUploadController.java
+++ src/main/java/com/cl/ysyd/controller/order/FtpFileUploadController.java
@@ -26,6 +26,8 @@ public class FtpFileUploadController {
 
     private static final String SUFFIXLIST = "jpg,png,pdf,jpeg,zip,7z,rar";
 
+    private static String filePath = "http://47.106.34.32/img/";
+
     @PostMapping("/uploadImg")
     @ApiOperation(value = "上传图片")
     public ResponseData<FileResDto> uploadImgs(@RequestParam("file")MultipartFile file) {
@@ -41,6 +43,9 @@ public class FtpFileUploadController {
         String fileName = file.getOriginalFilename();
         //获取文件后缀
         Assert.hasText(fileName, "文件名不能为空!");
+        filePath = filePath + fileName;
+        fileResDto.setFileName(fileName);
+        fileResDto.setFileUrl(filePath);
         String suffix=fileName.substring(fileName.lastIndexOf(".")+1);
         if(!SUFFIXLIST.contains(suffix.trim().toLowerCase())){
             throw new BusiException("只能上传jpg、png、pdf、jpeg、zip、7z、rar格式的文件！");
@@ -50,16 +55,13 @@ public class FtpFileUploadController {
         if (fileName.indexOf(" ") > 0) {
             throw new BusiException("文件名不能包含空格！请修改图片名称后重新上传！");
         }
-        String filePath = "http://47.106.34.32/img/";
+
         boolean flag= FtpFileUtil.uploadFile(file , fileName);
         if(flag){
-            filePath = filePath + fileName;
-            fileResDto.setFileName(fileName);
-            fileResDto.setFileUrl(filePath);
+            return new ResponseData<>(fileResDto);//该路径图片名称，前端框架可用ngnix指定的路径+filePath,即可访问到ngnix图片服务器中的图片
         }else{
             throw new BusiException("上传失败!");
         }
-        return new ResponseData<>(fileResDto);  //该路径图片名称，前端框架可用ngnix指定的路径+filePath,即可访问到ngnix图片服务器中的图片
     }
 
 }
